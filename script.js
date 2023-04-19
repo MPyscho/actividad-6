57d2fa194bb8b494d9bca8c4570a2f64b2e3f5f6
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-04-19 14:31:02 +0000

    variable numeros + console log

--- script.js
+++ script.js
@@ -12,9 +12,16 @@ function multiplicar(a, b) {
 
 function dividir(a, b) {
     if (b === 0) {
-        console.log("Error: No se puede dividir por cero.");
+        console.log("No se puede dividir por cero ");
         return;
     }
     return a / b;
 }
 
+var Numero1 = parseInt(prompt("Introduzca el primer número:"));
+var Numero2 = parseInt(prompt("Introduzca el segundo número:"));
+
+console.log("La suma es " + sumar(Numero1, Numero2));
+console.log("La resta es " + restar(Numero1, Numero2));
+console.log("La multiplicación es " + multiplicar(Numero1, Numero2));
+console.log("La división es " + dividir(Numero1, Numero2));
