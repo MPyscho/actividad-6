bca3400b29c1a80d650049ee02d19a42be392389
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-05-10 19:20:32 +0200

    a ver si así funciona ya

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var num1 = parseInt(prompt("Introduce un numero "));
+var num2 = parseInt(prompt("Introduce un segundo numero"));
 
 function sumar(num1, num2) {
   console.log(num1 + num2);
@@ -20,13 +22,7 @@ function dividir(num1, num2) {
     console.log("No se puede dividir entre cero.");
   }
 }
-
-
-var num1 = parseInt(prompt("Introduce un número "));
-var num2 = parseInt(prompt("Introduce un segundo número"));
-
-
-alert (" La suma de los dos es" + sumar(num1, num2));
-alert (" La resta de los dos es" + restar(num1, num2));
-alert (" La multiplicación de los dos es" + multiplicar(num1, num2));
-alert (" La división de los dos es" + dividir(num1, num2));
+alert(" La suma de los dos es" + sumar(num1, num2));
+alert(" La resta de los dos es" + restar(num1, num2));
+alert(" La multiplicacion de los dos es" + multiplicar(num1, num2));
+alert(" La division de los dos es" + dividir(num1, num2));
