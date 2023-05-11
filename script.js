eb2ca197ba22c4d6344d19b937e6248fcd49b743
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-05-11 10:48:36 +0200

    cambiar las variables creo qu ese es el fallo

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 var num1 = parseInt(prompt("Introduce un numero "));
 var num2 = parseInt(prompt("Introduce un segundo numero"));
 
-var numeroConvertido= Number(num1)
-var numeroConvertido= Number(num2)
+var numeroConvertido1= Number(num1)
+var numeroConvertido2= Number(num2)
 
-function sumar(num1, num2) {
-  return a= num1 + num2;
+function sumar(numeroConvertido1, numeroConvertido2) {
+  return (numeroConvertido1 + numeroConvertido2);
 }
 
-function restar(num1, num2) {
-  return a= (num1 - num2);
+function restar(numeroconvertido1, numeroConvertido2) {
+  return (numeroConvertido1 - numeroConvertido2);
 }
 
-function multiplicar(num1, num2) {
-  return a=(num1 * num2);
+function multiplicar(numeroConvertido1, numeroConvertido2) {
+  return (numeroConvertido1 * numeroConvertido2);
 }
 
 
-function dividir(num1, num2) {
+function dividir(numeroConvertido1, numeroConvertido2) {
   if (num2 !== 0) {
-    return a=(num1 / num2);
+    return (numeroConvertido1 / numeroConvertido2);
   } else {
     console.log("No se puede dividir entre cero.");
   }
 }
-alert(" La suma de los dos es" + sumar(num1, num2));
-alert(" La resta de los dos es" + restar(num1, num2));
-alert(" La multiplicacion de los dos es" + multiplicar(num1, num2));
-alert(" La division de los dos es" + dividir(num1, num2));
+alert(" La suma de los dos es" + sumar(numeroConvertido1, numeroConvertido2));
+alert(" La resta de los dos es" + restar(numeroConvertido1, numeroconvertido2));
+alert(" La multiplicacion de los dos es" + multiplicar(numeroConvertido1, numeroConvertido2));
+alert(" La division de los dos es" + dividir(numeroConvertido1, numeroConvertido2));
